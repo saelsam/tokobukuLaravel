0c1165831034004d060be58f3cc4676d0731f1cc
saelsam <saelsamuelrude@gmail.com>
2022-10-11 01:39:54 +0800

    Create header.blade.php

---
+++ posttest2/resources/views/components/header.blade.php
@@ -0,0 +1,15 @@
+<div class="container">
+<header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 memd-auto text-dark text-decoration-none">
+    <svg class="bi me-2" width="40" height="32">
+    <use xlink:href="#bootstrap"></use>
+    </svg>
+    <span class="fs-4" style="float: left;">Toko Buku </span>
+    </a>
+    <ul class="nav nav-pills" style="float: right;">
+        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">FAQ</a></li>
+        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">About</a></li>
+    </ul>
+</header>
+</div>
